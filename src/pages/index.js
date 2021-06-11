630973fa5fd1a9a6126e509562d24be2bd011d22
Gustavo Procopio <58342763+gustprocopio@users.noreply.github.com>
2021-06-11 09:15:29 -0300

    Add files via upload

---
+++ src/pages/index.js
@@ -0,0 +1,88 @@
+import React, { useState, useEffect, useRef } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+import Button from '@material-ui/core/Button'
+import Layout from '../components/Layout/index'
+import Metatags from '../components/Meta/index'
+
+import BlogCard from '../components/BlogSection/index'
+
+const useStyles = makeStyles(theme => ({
+  loadMore: {
+    display: 'flex',
+    justifyContent: 'center',
+    margin: theme.spacing(3),
+  },
+}))
+
+const Blog = ({ data, classes = useStyles() }) => {
+  const [loading, setLoading] = useState(false)
+  const [posts, setPosts] = useState(data || [])
+  const [search, setSearch] = useState('')
+  const [page, setPage] = useState(1)
+  const [refetch, setRefetch] = useState(true)
+  const isFirstRun = useRef(true)
+
+  useEffect(() => {
+    if (isFirstRun.current) {
+      isFirstRun.current = false
+      return
+    }
+    async function fetchPosts() {
+      setLoading(true)
+      const resp = await fetch(
+        `/api/posts?qs=${search}&orderby=relevance&page=${page}`
+      )
+      const json = await resp.json()
+      setPosts(posts.concat(json.data))
+      setLoading(false)
+    }
+    fetchPosts()
+  }, [page, refetch])
+
+  const handleSearch = async () => {
+    setPosts([])
+    setPage(1)
+    setRefetch(!refetch)
+  }
+
+  const loadMore = () => {
+    setPage(page + 1)
+  }
+
+  return (
+    <Layout
+      search={search}
+      handleSearch={handleSearch}
+      setSearch={setSearch}
+      home
+      loading={loading}
+    >
+      <Metatags
+        title="Mejor con Salud"
+        desc="Revista sobre buenos hábitos y cuidados para tu salud"
+      />
+      <Grid container spacing={4}>
+        {posts.map(post => (
+          <BlogCard key={post.id} post={post} />
+        ))}
+      </Grid>
+      <div className={classes.loadMore}>
+        <Button variant="contained" color="primary" onClick={loadMore}>
+          Load more
+        </Button>
+      </div>
+    </Layout>
+  )
+}
+
+export default Blog
+
+export async function getServerSideProps() {
+  const res = await fetch(
+    `https://api.beta.mejorconsalud.com/wp-json/mc/v1/posts?orderby=relevance`
+  )
+  const data = await res.json()
+
+  return { props: { data } }
+}
---
+++ src/pages/posts/[slug].jsx
@@ -0,0 +1,87 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+import { parseISO, format } from 'date-fns'
+import Head from 'next/head'
+import Layout from '../../components/Layout/index'
+import AuthorCard from '../../components/AuthorSection/index'
+import Metatags from '../../components/Meta/index'
+
+import { makeStyles } from '@material-ui/core/styles'
+import Chip from '@material-ui/core/Chip'
+
+const useStyles = makeStyles(() => ({
+  tagContainer: {
+    display: 'flex',
+  },
+  tag: {
+    margin: '0 5px',
+  },
+}))
+
+export default function Post({ post, classes = useStyles() }) {
+  const router = useRouter()
+
+  if (router.isFallback) {
+    return <div>loading...</div>
+  }
+
+  return (
+    <Layout loading={router.isFallback}>
+      <Metatags
+        title={`Mejor con Salud - ${post.title}`}
+        desc={post.headline}
+        canonical=""
+        image={post.featured_media.thumbnail}
+      />
+      <Head>
+        <title>Mejor con Salud - {post.title}</title>
+      </Head>
+      <article>
+        <h4>
+          <time dateTime={post.published}>
+            {format(parseISO(post.published), 'LLLL d, yyyy')}
+          </time>
+        </h4>
+        <h1>{post.title}</h1>
+
+        <h3>
+          Categoria: {post.categories.map(category => category.name).join(',')}
+        </h3>
+
+        <div dangerouslySetInnerHTML={{ __html: post.content }} />
+
+        <h4>Bibliography</h4>
+        <div dangerouslySetInnerHTML={{ __html: post.bibliography }} />
+
+        <div className={classes.tagContainer}>
+          {post.tags.map(tag => (
+            <Chip label={tag.name} key={tag.id} className={classes.tag} />
+          ))}
+        </div>
+
+        <AuthorCard author={post.author} />
+      </article>
+    </Layout>
+  )
+}
+
+export async function getStaticPaths() {
+  const resp = await fetch(
+    'https://api.beta.mejorconsalud.com/wp-json/mc/v1/posts?_fields=slug'
+  )
+  const posts = await resp.json()
+
+  return {
+    paths: posts.map(({ slug }) => ({ params: { slug: slug.toString() } })),
+    fallback: true,
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const res = await fetch(
+    `https://api.beta.mejorconsalud.com/wp-json/mc/v1/posts/${params.slug}`
+  )
+  const post = await res.json()
+
+  return { props: { post } }
+}
